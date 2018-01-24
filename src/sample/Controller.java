a2f841484144d39bcc37f01c3b4112056ed59dfc
Rashadse <rashadse@hotmail.com>
2018-01-24 14:35:54 +0100

    testing Junit

---
+++ src/sample/Controller.java
@@ -0,0 +1,14 @@
+package sample;
+
+public class Controller {
+
+    static int a = 0;
+    static int b = 15;
+
+    public static void main(String[] args){
+
+            System.out.println(a+b);
+
+    }
+
+}
---
+++ src/sample/ControllerTest.java
@@ -0,0 +1,11 @@
+package sample;
+
+import org.junit.jupiter.api.Test;
+
+
+class ControllerTest {
+    @Test
+    void main() {
+    }
+
+}
