f07842840b46dedb5413de09a836ab92470f9532
Showmick <showmick.kryotome@gmail.com>
2018-02-04 14:09:15 +0100

    Update CarPosition.java

--- src/sample/CarPosition.java
+++ src/sample/CarPosition.java
@@ -3,5 +3,8 @@ package sample;
 public class CarPosition {
     public int latitudinalPosition;
     public int longitudinalPosition;
-
+    
+    public Point getCarposition() {
+        return new Point(this.longitudinalPosition, this.latitudinalPosition);
+}
 }
