7696dd80fdd701e299afc5e5f71680f843002bf4
Showmick <showmick.kryotome@gmail.com>
2018-02-04 14:38:29 +0100

    Create StreetPosition.java

---
+++ src/StreetPosition.java
@@ -0,0 +1,11 @@
+ 
+ public int longidutanalposition;
+    public int latidudanolposition;
+    
+    public Tav(){
+    	this.latidudanolposition = 0;
+    	this.longidutanalposition = 0;
+    }
+    public Point currenposition(){
+    	
+    	return new Point(this.latidudanolposition,this.longidutanalposition);
