bd445191299d2ba3f5770123c6ff1faba66ec0ee
Rashadse <rashadse@hotmail.com>
2018-02-05 15:09:36 +0100

    tweaks

--- src/sample/CarMovement.java
+++ src/sample/CarMovement.java
@@ -5,96 +5,98 @@ public class CarMovement implements CarInterface {
     // default final value of 5 to be used each time the car moves
     final int distance = 5;
     // this boolean will be changed to false when the car reaches the end of the street
-     boolean streetEndNotReached = true;
+    boolean streetEndNotReached = true;
 
 
     int distanceMoved = 0;
-	
-	// this variable shows where lane the car is in,
-	//and it is initialised to 0, as the car starts at the right most lane.
-    static int lanePosition = 0; 
+
+    // this variable shows where lane the car is in,
+    //and it is initialised to 0, as the car starts at the right most lane.
+    int lanePosition = 0;
 
     @Override
-    public  void moveForward(int distance) {
+    public void moveForward(int distance) {
+
 
         if (distanceMoved < 95) {
 
             distanceMoved += distance;
         }
-        else{
+        if (distanceMoved >= 95){
             streetEndNotReached = false;
         }
-        System.out.println(distanceMoved);
     }
+
 
     /**
      * @return true if the lane is free, false otherwise
      */
     @Override
     public boolean leftLaneDetect(int[] sensorQuery_1, int[] sensorQuery_2) {
-    	
-        /** 
+
+        /**
          * These integer arrays hold the results from querying all sensors twice: 
          * R1, R2 and R3, L If either doesn't contain exactly 4 items, throw an
          * error. Otherwise, check all the values in the arrays.
          */
-    	
-    	 // Control boolean where return value is stored.
-    	boolean laneFree = false; 
-    	// Counter to track how many sensors detect a free lane in both queries.
-    	int sensorCounter = 0;  
-    	
-    	if (!((sensorQuery_1.length == 4) && (sensorQuery_2.length == 4))) {
-    		// We're not interested in covering this exception for now.
-    		throw new RuntimeException("Sensor readings incorrect.");
-    		
-    	} else {
-    		// Loop through both arrays.
-	    	for (int i = 0; i < 4; i++) {
-	    		// Same sensor in both queries works properly.
-	    		if ((sensorQuery_1[i] <= 50) && (sensorQuery_2[i] <= 50)) {
-	    			// Same sensor detects an obstacle in both queries.
-	    			if ((sensorQuery_1[i] <= 20) && (sensorQuery_2[i] <= 20)) {
-	    				// Lane is busy.
-	    				laneFree = false;
-	    				// Exit loop at this point to avoid incrementing sensorCounter.
-	    				break;
-	    			} else {
-	    				// Lane free, keep track of how many sensors confirm this.
-	    				sensorCounter++;
-	    			}
-	    			
-	    		} else {
-	    			// One or more sensors work incorrectly in one or both queries.
-	    			throw new RuntimeException("Obstacles out of range");
-	    		}	    		    		
-	    	};
-    	}
-    	// Two or more sensors detect a free lane in both queries.
-    	if (sensorCounter >= 2) {
-    		// Lane is free.
-    		laneFree = true;
-    	}
-    	// Return the status of the lane.
-		return laneFree;
-    	
+
+        // Control boolean where return value is stored.
+        boolean laneFree = false;
+        // Counter to track how many sensors detect a free lane in both queries.
+        int sensorCounter = 0;
+
+        if (!((sensorQuery_1.length == 4) && (sensorQuery_2.length == 4))) {
+            // We're not interested in covering this exception for now.
+            throw new RuntimeException("Sensor readings incorrect.");
+
+        } else {
+            // Loop through both arrays.
+            for (int i = 0; i < 4; i++) {
+                // Same sensor in both queries works properly.
+                if ((sensorQuery_1[i] <= 50) && (sensorQuery_2[i] <= 50)) {
+                    // Same sensor detects an obstacle in both queries.
+                    if ((sensorQuery_1[i] <= 20) && (sensorQuery_2[i] <= 20)) {
+                        // Lane is busy.
+                        laneFree = false;
+                        // Exit loop at this point to avoid incrementing sensorCounter.
+                        break;
+                    } else {
+                        // Lane free, keep track of how many sensors confirm this.
+                        sensorCounter++;
+                    }
+
+                } else {
+                    // One or more sensors work incorrectly in one or both queries.
+                    throw new RuntimeException("Obstacles out of range");
+                }
+            }
+            ;
+        }
+        // Two or more sensors detect a free lane in both queries.
+        if (sensorCounter >= 2) {
+            // Lane is free.
+            laneFree = true;
+        }
+        // Return the status of the lane.
+        return laneFree;
+
     }
 
     @Override
     public int changeLane() {
-	    lanePosition++;
+        lanePosition++;
         return 0;
     }
 
     @Override
     public int[] whereIs() {
         int[] arr = new int[2];
-    //longitudinalPosition
-	arr[0]  = distanceMoved;
-	// latitudinalPosition
-	arr[1]  = lanePosition;
-	    return arr;
-	   
+        //longitudinalPosition
+        arr[0] = distanceMoved;
+        // latitudinalPosition
+        arr[1] = lanePosition;
+        return arr;
+
     }
 
 }
--- testsrc/sample/CarMovementTest.java
+++ testsrc/sample/CarMovementTest.java
@@ -17,14 +17,21 @@ class CarMovementTest {
     }
 
     @Test
+    void whereIs() throws Exception {
+
+        int[] actual = classUnderTest.whereIs();
+        System.out.println(actual[0]);
+    }
+
+    @Test
     void moveForwardTest() throws Exception {
 
         boolean actual = classUnderTest.streetEndNotReached;
 
-    // check if the car has not reached the end of the track
-    assertTrue(actual, "boolean is not true");
-    // check if the car hasn't moved
-    assertEquals(0, classUnderTest.distanceMoved);
+        // check if the car has not reached the end of the track
+        assertTrue(actual, "boolean is not true");
+        // check if the car hasn't moved
+        assertEquals(0, classUnderTest.distanceMoved);
 
     }
 
@@ -32,7 +39,7 @@ class CarMovementTest {
     void moveForwardTest2() throws Exception {
 
         // move forward 5 meters
-        classUnderTest.moveForward(classUnderTest.distance);
+         classUnderTest.moveForward(classUnderTest.distance);
 
         boolean actual = classUnderTest.streetEndNotReached;
 
@@ -44,7 +51,7 @@ class CarMovementTest {
 
     @Test
     void moveForwardTest3() throws Exception {
-        // move forward 55 meters
+     //    move forward 55 meters
         for (int i = 0; i < 11; i++) {
             classUnderTest.moveForward(classUnderTest.distance);
         }
@@ -60,100 +67,94 @@ class CarMovementTest {
 
     @Test
     void moveForwardTest4() throws Exception {
-        // move forward 40 meters
-        int counter1 = 0;
-        for (int i = 0; i < 8; i++) {
+        // move forward 35 meters
+        for (int i = 0; i < 7; i++) {
             classUnderTest.moveForward(classUnderTest.distance);
-        counter1+=5;
         }
-        System.out.println(counter1);
         boolean actual = classUnderTest.streetEndNotReached;
 
-        // check if the car has reached the end of the track
-        assertFalse(actual, "boolean is not false");
         // check if the car has moved exactly 35 meters
         assertEquals(95, classUnderTest.distanceMoved);
 
+        // check if the car has reached the end of the track
+        assertFalse(actual, "boolean is not false");
     }
 
     @Test
     void leftLaneDetectTest1() throws Exception {
-    	// Test case 1.
-    	int[] Q1 = {55, 52, 52, 52};
-    	int[] Q2 = {55, 52, 52, 52};
-    	// Check the exception type, out of range expected.
-    	try {
-    		classUnderTest.leftLaneDetect(Q1, Q2);  // Act.
-    		fail("Expected a RuntimeException to be thrown");
-    	} catch (RuntimeException e) {
-    		assertEquals("Obstacles out of range", e.getMessage()); // Assert.
-    	}
+        // Test case 1.
+        int[] Q1 = {55, 52, 52, 52};
+        int[] Q2 = {55, 52, 52, 52};
+        // Check the exception type, out of range expected.
+        try {
+            classUnderTest.leftLaneDetect(Q1, Q2);  // Act.
+            fail("Expected a RuntimeException to be thrown");
+        } catch (RuntimeException e) {
+            assertEquals("Obstacles out of range", e.getMessage()); // Assert.
+        }
     }
-      
+
     @Test
     void leftLaneDetectTest2() throws Exception {
-    	//Test case 3.
-    	int[] Q1 = {25, 55, 25, 55};
-    	int[] Q2 = {20, 51, 51, 51};
-    	// Check the exception type, out of range expected.
-    	try {
-    		classUnderTest.leftLaneDetect(Q1, Q2);  // Act.
-    		fail("Expected a RuntimeException to be thrown");
-    	} catch (RuntimeException e) {
-    		assertEquals("Obstacles out of range", e.getMessage()); // Assert.
-    	}
+        //Test case 3.
+        int[] Q1 = {25, 55, 25, 55};
+        int[] Q2 = {20, 51, 51, 51};
+        // Check the exception type, out of range expected.
+        try {
+            classUnderTest.leftLaneDetect(Q1, Q2);  // Act.
+            fail("Expected a RuntimeException to be thrown");
+        } catch (RuntimeException e) {
+            assertEquals("Obstacles out of range", e.getMessage()); // Assert.
+        }
     }
-    
+
     @Test
     void leftLaneDetectTest3() throws Exception {
-    	// Test case 5.
-    	int[] Q1 = {30, 30, 30, 30};
-    	int[] Q2 = {35, 35, 35, 35};
-    	
-    	boolean laneFree = classUnderTest.leftLaneDetect(Q1, Q2);  // Act.
-    	// We expect the lane to be free.
-    	assertEquals(true, laneFree);  // Assert.
+        // Test case 5.
+        int[] Q1 = {30, 30, 30, 30};
+        int[] Q2 = {35, 35, 35, 35};
+
+        boolean laneFree = classUnderTest.leftLaneDetect(Q1, Q2);  // Act.
+        // We expect the lane to be free.
+        assertEquals(true, laneFree);  // Assert.
     }
-    
+
     @Test
     void leftLaneDetectTest4() throws Exception {
-    	//Test case 6.
-    	int[] Q1 = {22, 20, 25, 22};
-    	int[] Q2 = {25, 25, 25, 25};
-    	
-    	boolean laneFree = classUnderTest.leftLaneDetect(Q1, Q2);  // Act.
-    	// We expect the lane to be free.
-    	assertEquals(true, laneFree);  // Assert.
+        //Test case 6.
+        int[] Q1 = {22, 20, 25, 22};
+        int[] Q2 = {25, 25, 25, 25};
+
+        boolean laneFree = classUnderTest.leftLaneDetect(Q1, Q2);  // Act.
+        // We expect the lane to be free.
+        assertEquals(true, laneFree);  // Assert.
     }
-    
+
     @Test
     void leftLaneDetectTest5() throws Exception {
-    	// Test case 7.
-    	int[] Q1 = {25, 10, 25, 25};
-    	int[] Q2 = {25, 15, 25, 25};
-    	
-    	boolean laneFree = classUnderTest.leftLaneDetect(Q1, Q2);  // Act.
-    	// We expect the lane to be busy.
-    	assertEquals(false, laneFree);  // Assert.
+        // Test case 7.
+        int[] Q1 = {25, 10, 25, 25};
+        int[] Q2 = {25, 15, 25, 25};
+
+        boolean laneFree = classUnderTest.leftLaneDetect(Q1, Q2);  // Act.
+        // We expect the lane to be busy.
+        assertEquals(false, laneFree);  // Assert.
     }
-    
+
     @Test
     void leftLaneDetectTest6() throws Exception {
-    	//Test case 8.
-    	int[] Q1 = {15, 15, 22, 22};
-    	int[] Q2 = {15, 10, 22, 22};
-    	
-    	boolean laneFree = classUnderTest.leftLaneDetect(Q1, Q2);  // Act.
-    	// We expect the lane to be busy.
-    	assertEquals(false, laneFree);  // Assert.
+        //Test case 8.
+        int[] Q1 = {15, 15, 22, 22};
+        int[] Q2 = {15, 10, 22, 22};
+
+        boolean laneFree = classUnderTest.leftLaneDetect(Q1, Q2);  // Act.
+        // We expect the lane to be busy.
+        assertEquals(false, laneFree);  // Assert.
     }
 
     @Test
     void changeLane() throws Exception {
     }
 
-    @Test
-    void whereIs() throws Exception {
-    }
 
 }
