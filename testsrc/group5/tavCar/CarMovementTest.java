3487c4530820d0d85ce07adb9585a61f0a6f245e
Kosara Golemshinska <gusgolko@student.gu.se>
2018-02-26 16:10:05 +0100

    removed unused code

--- testsrc/group5/tavCar/CarMovementTest.java
+++ testsrc/group5/tavCar/CarMovementTest.java
@@ -27,8 +27,6 @@ public class CarMovementTest {
     	MockitoAnnotations.initMocks(this);
     }
 
-
-
     // This method checks if the car moves 55 meters when moveForward() is invoked 11 times
     @Test
     void moveForwardTest1() throws Exception {
@@ -91,11 +89,8 @@ public class CarMovementTest {
 
     @Test
     void leftLaneDetectTest1() throws Exception {
-        // Test case 1.
-        int[] Q1 = {55, 52, 52, 52};
-        int[] Q2 = {55, 52, 52, 52};
-        
-        CarMovement testClass = mock(CarMovement.class);  // Mock.
+     
+    	CarMovement testClass = mock(CarMovement.class);  // Mock.
         
         testClass.leftLaneDetect();  // Act.
         
@@ -106,9 +101,6 @@ public class CarMovementTest {
 
     @Test
     void leftLaneDetectTest2() throws Exception {
-        //Test case 3.
-        int[] Q1 = {45, 55, 55, 60};
-        int[] Q2 = {47, 51, 51, 60};
         
         CarMovement testClass = mock(CarMovement.class);  // Mock.
         
@@ -121,9 +113,6 @@ public class CarMovementTest {
 
     @Test
     void leftLaneDetectTest3() throws Exception {
-        // Test case 5.
-        int[] Q1 = {45, 10, 10, 30};
-        int[] Q2 = {45, 10, 10, 31};
         
         CarMovement testClass = mock(CarMovement.class);  // Mock.
         when(testClass.leftLaneDetect()).thenReturn(true);  // Stub.
@@ -136,9 +125,6 @@ public class CarMovementTest {
 
     @Test
     void leftLaneDetectTest4() throws Exception {
-        //Test case 6.
-        int[] Q1 = {22, 24, 25, 45};
-        int[] Q2 = {25, 25, 25, 45};
 
         CarMovement testClass = mock(CarMovement.class);  // Mock.
         when(testClass.leftLaneDetect()).thenReturn(true);  // Stub.
@@ -151,9 +137,6 @@ public class CarMovementTest {
 
     @Test
     void leftLaneDetectTest5() throws Exception {
-        // Test case 7.
-        int[] Q1 = {25, 10, 9, 25};
-        int[] Q2 = {25, 11, 9, 25};
 
         CarMovement testClass = mock(CarMovement.class);  // Mock.
         when(testClass.leftLaneDetect()).thenReturn(false);  // Stub.
@@ -166,9 +149,6 @@ public class CarMovementTest {
 
     @Test
     void leftLaneDetectTest6() throws Exception {
-        //Test case 8.
-        int[] Q1 = {15, 5, 5, 22};
-        int[] Q2 = {15, 5, 6, 22};
 
         CarMovement testClass = mock(CarMovement.class);  // Mock.
         when(testClass.leftLaneDetect()).thenReturn(false);  // Stub.
