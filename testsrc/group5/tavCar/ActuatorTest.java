776e26d75116e05d2a046e04b6eaca267271c2ab
Rashadse <rashadse@hotmail.com>
2018-02-27 11:08:12 +0100

    git ignore and initial actuator test

---
+++ testsrc/group5/tavCar/ActuatorTest.java
@@ -0,0 +1,29 @@
+package group5.tavCar;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ActuatorTest {
+
+    @Test
+    void moveForward() {
+    }
+
+    @Test
+    void moveForward1() {
+    }
+
+    @Test
+    void setWheelAngle() {
+    }
+
+    @Test
+    void getDistance() {
+    }
+
+    @Test
+    void getLanePosition() {
+    }
+
+}
