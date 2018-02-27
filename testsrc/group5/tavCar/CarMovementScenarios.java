8bb55fd93a5b4ccfbf93f840d70f712201c62bfa
Kosara Golemshinska <gusgolko@student.gu.se>
2018-02-27 16:35:06 +0100

    scenario 7

--- testsrc/group5/tavCar/CarMovementScenarios.java
+++ testsrc/group5/tavCar/CarMovementScenarios.java
@@ -215,6 +215,113 @@ public class CarMovementScenarios {
         // Assert distance traveled.
         assertEquals(45, car.whereIs().distance);
         
+        // Radar sensors pick up a car in the left lane.
+        when(radarSensors.Read()).thenReturn(emptyLaneRadarQuery);
+        // Lidar sensor picks up a car ahead.
+        when(lidarSensor.Read()).thenReturn(busyLaneLidarQuery);
+
+        // Act.
+        car.changeLane();
+
+        // Verify we're calling the mock.
+        verify(actuator, atLeastOnce()).moveForward();
+
+        // We assert the car's in the middle lane.
+        assertEquals(1, car.whereIs().lanePosition);
+        
+        // Moves 15 meters without changing lanes.
+        for(int i = 0; i < 2; i++ ) {
+            car.moveForward();
+        }
+        // Verify we actually call the actuator.
+        verify(actuator, atLeastOnce()).moveForward();
+
+        // Assert distance traveled.
+        assertEquals(60, car.whereIs().distance);
+        
+        // Radar sensors pick up a car in the left lane.
+        when(radarSensors.Read()).thenReturn(busyLaneRadarQuery);
+        // Lidar sensor picks up a car far ahead.
+        when(lidarSensor.Read()).thenReturn(emptyLaneLidarQuery);
+
+        // Act.
+        car.changeLane();
+
+        // Verify we're calling the mock.
+        verify(actuator, atLeastOnce()).moveForward();
+
+        // We assert the car's in the same lane.
+        assertEquals(1, car.whereIs().lanePosition);
+        
+        // Moves 15 meters without changing lanes.
+        for(int i = 0; i < 2; i++ ) {
+            car.moveForward();
+        }
+        // Verify we actually call the actuator.
+        verify(actuator, atLeastOnce()).moveForward();
+
+        // Assert distance traveled.
+        assertEquals(75, car.whereIs().distance);
+        
+        // Radar sensors pick up an empty lane.
+        when(radarSensors.Read()).thenReturn(emptyLaneRadarQuery);
+        // Lidar sensor picks up an empty lane.
+        when(lidarSensor.Read()).thenReturn(emptyLaneLidarQuery);
+
+        // Act.
+        car.changeLane();
+
+        // Verify we're calling the mock.
+        verify(actuator, atLeastOnce()).moveForward();
+
+        // We assert the car's in the left-most lane.
+        assertEquals(2, car.whereIs().lanePosition);
+        
+        // Move forward until end of road.
+        for(int i = 0; i < 3; i++){
+            car.moveForward();
+        }	
+    }
+    
+    @Test
+    void scenario7() {
+    	
+        // Starts at the beginning of the street.
+        // Moves 25 meters without changing lanes.
+        for(int i = 0; i < 4; i++ ) {
+            car.moveForward();
+        }
+        // Verify we actually call the actuator.
+        verify(actuator, atLeastOnce()).moveForward();
+
+        // Assert distance traveled.
+        assertEquals(20, car.whereIs().distance);
+        
+        // Radar sensors pick up a car in the left lane.
+        when(radarSensors.Read()).thenReturn(busyLaneRadarQuery);
+        // Lidar sensor picks up a car far ahead.
+        when(lidarSensor.Read()).thenReturn(emptyLaneLidarQuery);
+
+        // Act.
+        car.changeLane();
+
+        // Verify we're calling the mock.
+        verify(actuator, atLeastOnce()).moveForward();
+
+        // We assert the car's in the same lane.
+        assertEquals(0, car.whereIs().lanePosition);
+
+        // Move forward 25 meters
+        for(int i = 0; i < 4; i++){
+            car.moveForward();
+        }	
+        
+        // Verify we actually call the actuator.
+        verify(actuator, atLeastOnce()).moveForward();
+
+        // Assert distance traveled.
+        assertEquals(45, car.whereIs().distance);
+        
         // Radar sensors pick up a car one lane over.
         when(radarSensors.Read()).thenReturn(emptyLaneRadarQuery);
         // Lidar sensor picks up a car ahead.
@@ -263,8 +370,13 @@ public class CarMovementScenarios {
         // Assert distance traveled.
         assertEquals(75, car.whereIs().distance);
         
-        // Radar sensors pick up a car in the left lane.
+        // Radar sensors pick up an empty lane.
         when(radarSensors.Read()).thenReturn(emptyLaneRadarQuery);
+        // Lidar sensor picks up a car far ahead.
+        when(lidarSensor.Read()).thenReturn(emptyLaneLidarQuery);
+        
+        // Radar sensors pick up a car in the left lane.
+        when(radarSensors.Read()).thenReturn(busyLaneRadarQuery);
         // Lidar sensor picks up a car far ahead.
         when(lidarSensor.Read()).thenReturn(emptyLaneLidarQuery);
 
@@ -274,8 +386,8 @@ public class CarMovementScenarios {
         // Verify we're calling the mock.
         verify(actuator, atLeastOnce()).moveForward();
 
-        // We assert the car's in the left-most lane.
-        assertEquals(2, car.whereIs().lanePosition);
+        // We assert the car's in the same lane.
+        assertEquals(1, car.whereIs().lanePosition);
         
         // Move forward until end of road.
         for(int i = 0; i < 3; i++){
